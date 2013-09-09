6c5f4d9f3b7eeafc3831bbb35e6cf4966875e17f
Anna Merrill <anna.e.larson@gmail.com>
2013-09-09 13:32:19 -0500

    Before updating CSMSL

---
+++ NonQuantifiableType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coon.NeuQuant
+{
+    
+    [Flags]
+    public enum NonQuantifiableType
+    {
+        Unspecified = 0,
+        NoLabel = 1,
+        NotResolvable = 2,
+        NoPeaksFoundWithinTolerance = 4,
+        WrongIsotopeDistribution = 8,
+        ImproperSpacing = 16,
+        InconsistentPeakPatterns = 32,
+        WrongElutionProfiles = 64,
+        NotEnoughMeasurements = 128,
+        Quantified = 256,
+    }
+}
---
+++ PeakPattern.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coon.NeuQuant
+{
+   class PeakPattern
+    {
+        public int NumPeaks;
+        public int PatternID;
+        public double RawScore
+        {
+            get
+            {
+                double score = 0;
+                int peakCount = 0;
+                if (PatternPairs != null)
+                {
+                    foreach (Pair pair in PatternPairs)
+                    {
+                        for (int i = 0; i < ParentPeptide.numChannels; i++)
+                        {
+                            for (int j = 0; j < ParentPeptide.numIsotopes; j++)
+                            {
+                                if (pair.peaks[i, j] != null)
+                                {
+                                    peakCount++;
+                                    score += pair.peaks[i, j].GetSignalToNoise() * (2 - j / 2);
+                                }
+                            }
+                        }
+                    }
+                    score = score / peakCount;
+                }
+                return score;
+            }
+        }
+        public double NormalizedScore { set; get; }
+        public PeptideID ParentPeptide;
+        public List<Pair> PatternPairs { set; get; }
+        int PairCount
+        {
+            get
+            {
+                if (PatternPairs != null) return PatternPairs.Count;
+                else return 0;
+            }
+        }
+
+        public PeakPattern(int numPeaks, int patternID, PeptideID parentPeptide, Pair pair = null)
+        {
+            NumPeaks = numPeaks;
+            PatternID = patternID;
+            ParentPeptide = parentPeptide;
+
+            PatternPairs = new List<Pair>();
+
+            if (pair != null)
+            {
+                PatternPairs.Add(pair);
+            }
+
+        }
+
+        public void setNormalizedScore(double normalizationValue)
+        {
+            NormalizedScore = PairCount + (RawScore / normalizationValue);
+        }
+
+    }
+}
